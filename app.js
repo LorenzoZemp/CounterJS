79801c58236c991eedac06c96d09f029cb7736bb
Lorenzo <lorenzozemp@icloud.com>
2022-10-19 15:32:19 +1300

    finished implementation

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 // Getting interactive elements
 const counter = document.getElementById("counter");
-const decBtn = document.getElementById("dec");
-const resBtn = document.getElementById("res");
-const incBtn = document.getElementById("inc");
 
-const buttons = [decBtn, resBtn, incBtn];
+//array of all the buttons
+const buttons = document.querySelectorAll(".btn");
+
+// set initial count
+var count = 0;
 
 buttons.forEach((element) => {
   element.addEventListener("click", function () {
-    console.log(`clicked on ${element.id}`);
+    // console.log(`clicked on ${element.id}`);
+    switch (element.id) {
+      case "dec":
+        count -= 1;
+        break;
+      case "res":
+        count = 0;
+        break;
+      case "inc":
+        count += 1;
+        break;
+    }
+    if (count == -1) {
+      counter.style.color = "var(--red)";
+    }
+    if (count == 1) {
+      counter.style.color = "var(--green)";
+    }
+    if (count == 0) {
+      counter.style.color = "black";
+    }
+    // console.log(count);
+    counter.textContent = count;
   });
 });
